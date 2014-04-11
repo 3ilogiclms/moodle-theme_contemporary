2bb4c48373aa046f24d09af1f49772d3ae417fe1
3i Logic LMS <azmat@3ilogic.com>
2014-04-11 18:19:40 +0500

    Added Responsive
    Added Meta element to make responsive design.

--- layout/general.php
+++ layout/general.php
@@ -50,6 +50,7 @@ echo $OUTPUT->doctype() ?>
 <head>
     <title><?php echo $PAGE->title ?></title>
     <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme')?>" />
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <?php echo $OUTPUT->standard_head_html() ?>
 </head>
 <table width="100%">
