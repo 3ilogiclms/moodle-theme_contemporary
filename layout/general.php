30913e371f97fed1fba793d0f4eaf980dc5466f8
3i Logic LMS <azmat@3ilogic.com>
2014-08-11 16:39:00 +0500

    Upgraded on Moodle 2.7 with new features

--- layout/general.php
+++ layout/general.php
@@ -1,6 +1,5 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
-//
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -10,7 +9,6 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-//
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -45,125 +43,126 @@ if ($showsidepre && !$showsidepost) {
 if ($hascustommenu) {
     $bodyclasses[] = 'has_custom_menu';
 }
-echo $OUTPUT->doctype() ?>
+echo $OUTPUT->doctype()
+?>
 <html <?php echo $OUTPUT->htmlattributes() ?>>
-<head>
-    <title><?php echo $PAGE->title ?></title>
-    <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme')?>" />
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <?php echo $OUTPUT->standard_head_html() ?>
-</head>
-<table width="100%">
-<tr><td>
+    <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title><?php echo $PAGE->title ?></title>
+        <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme') ?>" />
+<?php echo $OUTPUT->standard_head_html() ?>
+    </head>
+    <table width="100%">
+        <tr><td>
 
-<body id="<?php p($PAGE->bodyid) ?>" class="<?php p($PAGE->bodyclasses.' '.join(' ', $bodyclasses)) ?>">
+        <body id="<?php p($PAGE->bodyid) ?>" class="<?php p($PAGE->bodyclasses . ' ' . join(' ', $bodyclasses)) ?>">
 <?php echo $OUTPUT->standard_top_of_body_html() ?>
 
-<div id="page">
-    <div id="wrapper" class="clearfix">
+            <div id="page">
+                <div id="wrapper" class="clearfix">
 <?php if ($hasheading || $hasnavbar) { ?>
 
-    <div id="page-header" class="clearfix">
+                        <div id="page-header" class="clearfix">
 
-            <?php
-    if ($hasheading) {
-        ?>
-                <h1 class="headermain">
-				
-				<?php // Comment echo $PAGE->heading ? ?></h1>
-                <div class="headermenu">
-                    <?php
-                        echo $OUTPUT->login_info();
-        if (!empty($PAGE->layout_options['langmenu'])) {
-                               echo $OUTPUT->lang_menu();
-        }
-                           echo $PAGE->headingmenu
+                            <?php
+                            if ($hasheading) {
+                                ?>
+                                <h1 class="headermain">
+
+                                    <?php // Comment echo $PAGE->heading ?  ?></h1>
+                                <div class="headermenu">
+                                    <?php
+                                    echo $OUTPUT->login_info();
+                                    if (!empty($PAGE->layout_options['langmenu'])) {
+                                        echo $OUTPUT->lang_menu();
+                                    }
+                                    echo $PAGE->headingmenu
+                                    ?>
+                                </div>
+                                <?php }
+                            ?>
+
+                        </div>
+
+                        <?php if ($hascustommenu) { ?>
+                            <div id="custommenu"><?php echo $custommenu; ?></div>
+                            <?php }
+                        ?>
+
+                        <?php if ($hasnavbar) { ?>
+                            <div class="navbar clearfix">
+                                <div class="breadcrumb"><?php echo $OUTPUT->navbar(); ?></div>
+                                <div class="navbutton"> <?php // Comment echo $PAGE->button; ?  ?></div>
+                            </div>
+                            <?php
+                        }
+                    }
                     ?>
-                </div>
-            <?php
-    }?>
 
-    </div>
+                    <!-- END OF HEADER -->
 
-    <?php
-    if ($hascustommenu) { ?>
-        <div id="custommenu"><?php echo $custommenu; ?></div>
-    <?php 
-    } ?>
+                    <div id="page-content-wrapper">
+                        <div id="page-content">
+                            <div id="region-main-box">
+                                <div id="region-post-box">
 
-    <?php
-    if ($hasnavbar) { ?>
-        <div class="navbar clearfix">
-            <div class="breadcrumb"><?php echo $OUTPUT->navbar(); ?></div>
-            <div class="navbutton"> <?php echo $PAGE->button; ?></div>
-        </div>
-    <?php
-    }
-}
-?>
+                                    <div id="region-main-wrap">
+                                        <div id="region-main">
+                                            <div class="region-content">
+                                                <?php echo core_renderer::MAIN_CONTENT_TOKEN ?>
+                                            </div>
+                                        </div>
+                                    </div>
 
-<!-- END OF HEADER -->
+                                    <?php if ($hassidepre) { ?>
+                                        <div id="region-pre" class="block-region">
+                                            <div class="region-content">
+                                                <?php echo $OUTPUT->blocks_for_region('side-pre') ?>
+                                            </div>
+                                        </div>
+                                        <?php }
+                                    ?>
 
-<div id="page-content-wrapper">
-    <div id="page-content">
-        <div id="region-main-box">
-            <div id="region-post-box">
+                                    <?php if ($hassidepost) { ?>
+                                        <div id="region-post" class="block-region">
+                                            <div class="region-content">
+                                                <?php echo $OUTPUT->blocks_for_region('side-post') ?>
+                                            </div>
+                                        </div>
+                                        <?php }
+                                    ?>
 
-                <div id="region-main-wrap">
-                    <div id="region-main">
-                        <div class="region-content">
-                            <?php echo core_renderer::MAIN_CONTENT_TOKEN ?>
+                                </div>
+                            </div>
                         </div>
                     </div>
+
                 </div>
 
-                <?php if ($hassidepre) { ?>
-                <div id="region-pre" class="block-region">
-                    <div class="region-content">
-                        <?php echo $OUTPUT->blocks_for_region('side-pre') ?>
+                <!-- START OF FOOTER -->
+                <?php if ($hasfooter_layout) {
+                    ?>
+
+                    <div id="page-footer" class="clearfix">
+                        <?php
+                        if ($hasfooter) {
+                            echo $PAGE->theme->settings->footertext;
+                        } else {
+                            ?>
+                            <p class="helplink"><?php //echo page_doc_link(get_string('moodledocslink'))  ?></p>
+                            <?php
+                            echo $OUTPUT->login_info();
+                            //	echo $OUTPUT->home_link();
+                            echo $OUTPUT->standard_footer_html();
+                        }
+                        ?>
                     </div>
-                </div>
-                <?php 
-} ?>
-
-                <?php if ($hassidepost) { ?>
-                <div id="region-post" class="block-region">
-                    <div class="region-content">
-                        <?php echo $OUTPUT->blocks_for_region('side-post') ?>
-                    </div>
-                </div>
-                <?php 
-} ?>
-
+                    <?php }
+                ?>
             </div>
-        </div>
-    </div>
-</div>
-
-    </div>
-
-<!-- START OF FOOTER -->
-    <?php if ($hasfooter_layout) { 
-	?>
-	
-    <div id="page-footer" class="clearfix">
-	<?php
-	if($hasfooter) {
-	   echo $PAGE->theme->settings->footertext;
-	} else { ?>
-        <p class="helplink"><?php echo page_doc_link(get_string('moodledocslink')) ?></p>
-        <?php
-        echo $OUTPUT->login_info();
-		echo $OUTPUT->home_link();
-    	echo $OUTPUT->standard_footer_html();
-    } ?>
-	</div>
-	<?php
-}?>
-</div>
 <?php echo $OUTPUT->standard_end_of_body_html() ?>
-</body>
-</td>
+        </body>
+    </td>
 </tr>
 </table>
 </html>
