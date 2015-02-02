93be64d0a145aed371e3b15b01e5e9b71f5020ad
3i Logic LMS <lms@3ilogic.com>
2015-02-02 14:57:42 +0500

    Update version information

--- version.php
+++ version.php
@@ -1,27 +1,44 @@
 <?php
 
 // This file is part of Moodle - http://moodle.org/
+//
+
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
+//
+
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
+//
+
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+
+
 /**
+
  * Theme version info
+
+ *
+
  * @package    theme
+
  * @subpackage contemporary
- * @copyright � 2012 - 2014 | 3i Logic (Pvt) Ltd.
+
+ * @copyright � 2012 - 2013 | 3i Logic (Pvt) Ltd.
+
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+
  */
 defined('MOODLE_INTERNAL') || die;
-$plugin->version = 2014080703; // The current module version (Date: YYYYMMDDXX)
+$plugin->version = 2015012900; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2010112400; // Requires this Moodle version
-$plugin->release = '2.4';
-$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '2.7';
+$plugin->maturity = MATURITY_ALPHA;
 $plugin->component = 'theme_contemporary'; // Full name of the plugin (used for diagnostics)
+
